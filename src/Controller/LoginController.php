d85e002cbed65b2b4957b9c83613e784f2a9c01a
Krzysztof <k.konwinski@onet.pl>
2022-11-02 20:41:53 +0100

    lgoin and logout actions

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,5 +20,15 @@ class LoginController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): void
+    {
+        // controller can be blank: it will never be called!
+        throw new \RuntimeException('Don\'t forget to activate logout in security.yaml');
     }
 }
