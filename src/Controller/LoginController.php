013050bda07cc8f44fd1ec295f27cf6587c26349
Krzysztof <k.konwinski@onet.pl>
2022-11-02 22:17:52 +0100

    register modal and change path after login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -16,7 +17,7 @@ class LoginController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
 
         $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('login/index.html.twig', [
+        return $this->render('login/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
@@ -26,9 +27,8 @@ class LoginController extends AbstractController
      * @throws Exception
      */
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
-    public function logout(): void
+    public function logout(): RedirectResponse
     {
-        // controller can be blank: it will never be called!
-        throw new \RuntimeException('Don\'t forget to activate logout in security.yaml');
+        return $this->redirect('app_login');
     }
 }
